7eb88d3b9900949692c64ecf42cf9d063f9c07c1
root <root@void.localdomain>
2020-11-27 07:27:03 +0900

    v1 struct

---
+++ go_struct_02.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type subscriber struct {
+	name   string
+	rate   int
+	active bool
+}
+func printInfo(s subscriber) {
+	fmt.Println("Name : ", s.name)
+	fmt.Println("Monthly rate : ", s.rate)
+	fmt.Println("Active? ", s.active)
+}
+func defaultSubscriber(name string) subscriber {
+	var s subscriber
+	s.name = name
+	s.rate = 5000
+	s.active = true
+	return s
+}
+func main() {
+	s1 := defaultSubscriber("kim")
+	s1.rate = 4500
+	printInfo(s1)
+
+
+	s2 := defaultSubscriber("park")
+	printInfo(s2)
+}
