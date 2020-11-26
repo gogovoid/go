dbdaa4ac31a98c8ba0b374dc30a86b7aeb2429a2
root <root@void.localdomain>
2020-11-27 07:46:36 +0900

    v3 struct

--- go_struct_02.go
+++ go_struct_02.go
@@ -7,24 +7,25 @@ type subscriber struct {
 	rate   int
 	active bool
 }
-func printInfo(s subscriber) {
+func printInfo(s *subscriber) {
 	fmt.Println("Name : ", s.name)
 	fmt.Println("Monthly rate : ", s.rate)
 	fmt.Println("Active? ", s.active)
 }
-func defaultSubscriber(name string) subscriber {
+func defaultSubscriber(name string) *subscriber {
 	var s subscriber
 	s.name = name
 	s.rate = 5000
 	s.active = true
-	return s
+	return &s
 }
 func applyDiscount(s *subscriber) {
 	s.rate = 4000
 }
 func main() {
 	s1 := defaultSubscriber("kim")
-	s1.rate = 4500
+	//s1.rate = 4500
+	applyDiscount(s1)
 	printInfo(s1)
 
 	s2 := defaultSubscriber("park")
